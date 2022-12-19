c8dea1010efbb0160a94c9c764414b8738e86682
dwiprayoga10 <113033658+dwiprayoga10@users.noreply.github.com>
2022-12-20 05:11:55 +0700

    Delete script.js

--- Tugas Akhir/undangan1/img/script.js
+++
@@ -1,8 +0,0 @@
-var myVideo = document.getElementById("video1"); 
-        function play() { 
-            myVideo.play();
-        }
-        function pause() {
-            myVideo.pause();
-        }
-    
